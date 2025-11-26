38e4dd2a05895728b753ed3190fc87e5dbf45f84
Ken Jin <kenjin4096@gmail.com>
2025-11-26 16:20:57 +0000

    refactor and account for file read time

--- search.py
+++ search.py
@@ -3,13 +3,13 @@ import time
 import sys
 import subprocess
 
-N_ITERS = 23
+N_ITERS = 25
 
 MAX_NO_PROGRESS_THRESHOLD = 5
 
-MAX_LOOPS_SUPPORTED = 30000
+MAX_LOOPS_SUPPORTED = 3000
 
-NUM_OUTER_ITERATIONS = 10
+NUM_OUTER_ITERATIONS = 20
 
 def disable_turbo_boost():
     os.system('echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo')
@@ -29,14 +29,15 @@ EXTRA_OPTS = f"--jit counterfile={LOOP_FILENAME}"
 
 
 # add/subtract by this much from the loop file.
-PERTURB_BY = 12
+PERTURB_BY = 300
 
+PYPY_DEFAULT_BRIDGE = 200
 import random
 
-def perturb_by(line):
+def perturb(line, by):
     res = []
     for num in line.split(','):
-        offset = random.choice(range(-PERTURB_BY//2, PERTURB_BY))
+        offset = random.choice(range(-by, by))
         res.append(f"{max(int(num) + offset, 1)}")
     return ",".join(res)
 
@@ -48,12 +49,12 @@ def mutate():
         function = contents[1]
         bridges = contents[2]
     with open(LOOP_FILENAME, "w") as fp:
-        fp.write(perturb_by(loop))
+        fp.write(perturb(loop, PERTURB_BY))
         fp.write("\n")
-        fp.write(perturb_by(function))
+        fp.write(perturb(function, PERTURB_BY))
         fp.write("\n")
-        fp.write(perturb_by(bridges))
-        fp.write("\n")   
+        fp.write(perturb(bridges, PERTURB_BY // 4))
+        fp.write("\n")
 
 import re
 AVERAGE_PAT = re.compile(f".+ average: (\d+)")
@@ -66,7 +67,7 @@ AWFY_BENCHMARKS = {
     "Json": 100,
     "CD": 250,
     "Havlak": 1500,
-    # "Bounce": 1500,
+    # "Bounce": 1500, # too short
     "List": 1500,
     "Mandelbrot": 750,
     "NBody": 500000,
@@ -81,52 +82,61 @@ AWFY_BENCHMARKS = {
 STATS_FILE = "stats.txt"
 STATS_FILE_SORTED = "stats-sorted.txt"
 
+
+def single_step(bench_name, inner_iterations, pypy_startup_time, best_time_so_far):
+    BEST_LOOP_FILENAME = f"loops_best_{bench_name}"
+    no_progress_counter = 0
+    try:
+        while True:
+            start = time.time()
+            os.system(f"{PYPY_PATH} {EXTRA_OPTS} -c 'pass'")
+            end = time.time()
+            pypy_startup_plus_readfile = end - start
+            pypy_readfile_time = pypy_startup_plus_readfile - pypy_startup_time
+            start = time.time()
+            os.system(f"{PYPY_PATH} {EXTRA_OPTS} src/test/are-we-fast-yet/Python/harness.py {bench_name} {NUM_OUTER_ITERATIONS} {inner_iterations}")
+            end = time.time()
+            time_taken = end - start - pypy_readfile_time
+            print(time_taken)
+            if time_taken < best_time_so_far * 0.99:
+                best_time_so_far = time_taken
+                no_progress_counter = 0
+                print(f"BETTER TIME FOUND: {best_time_so_far}")
+                os.system(f"cp {LOOP_FILENAME} {BEST_LOOP_FILENAME}")
+            else:
+                no_progress_counter += 1
+                if no_progress_counter >= MAX_NO_PROGRESS_THRESHOLD:
+                    raise NoProgressException()
+            mutate()
+    except NoProgressException:
+        # reset the loop to the best one, and start mutating from there.
+        os.system(f"cp {BEST_LOOP_FILENAME} {LOOP_FILENAME}")
+        mutate()
+    return best_time_so_far
+
 def minimize(bench_name, inner_iterations):
     best_time_so_far = float('+inf')
-    BEST_LOOP_FILENAME = f"loops_best_{bench_name}"
-    print(bench_name)    
+    print(bench_name)
+    start = time.time()
+    os.system(f"{PYPY_PATH} -c 'pass'")
+    end = time.time()
+    pypy_startup_time = end - start
+    initialize_loopfile()
     for i in range(N_ITERS):
         print(i)
-        no_progress_counter = 0
-        try:
-            while True:
-                start = time.time()
-                os.system(f"{PYPY_PATH} {EXTRA_OPTS} src/test/are-we-fast-yet/Python/harness.py {bench_name} {NUM_OUTER_ITERATIONS} {inner_iterations}")
-                end = time.time()
-                time_taken = end - start
-                print(time_taken)
-                if time_taken < best_time_so_far * 0.99:
-                    best_time_so_far = time_taken
-                    no_progress_counter = 0
-                    print(f"BETTER TIME FOUND: {best_time_so_far}")
-                    os.system(f"cp {LOOP_FILENAME} {BEST_LOOP_FILENAME}")
-                else:
-                    no_progress_counter += 1
-                    if no_progress_counter >= MAX_NO_PROGRESS_THRESHOLD:
-                        raise NoProgressException()
-                mutate()
-        except NoProgressException:
-            # reset the loop to the best one, and start mutating from there.
-            os.system(f"cp {BEST_LOOP_FILENAME} {LOOP_FILENAME}")
-            mutate()
-    with open(STATS_FILE, "a") as fp:
-        # Note: no extra opts here, so it's just default pypy!
-        start = time.time()
-        os.system(f"{PYPY_PATH} src/test/are-we-fast-yet/Python/harness.py {bench_name} {NUM_OUTER_ITERATIONS} {inner_iterations}")
-        end = time.time()
-        time_taken = end - start
-        pct_reduction = (best_time_so_far - time_taken) / time_taken * 100
-        fp.write(f"{bench_name},{time_taken},{best_time_so_far},{pct_reduction}\n")
-
+        best_time_so_far = single_step(bench_name, inner_iterations, pypy_startup_time, best_time_so_far)
 
 
 def initialize_loopfile():
     with open(LOOP_FILENAME, "w") as fp:
-        fp.write(','.join([f"{PERTURB_BY}"] * MAX_LOOPS_SUPPORTED))
+        # pypy default loop counter
+        fp.write(','.join([f"{1039}"] * MAX_LOOPS_SUPPORTED))
         fp.write("\n")
-        fp.write(','.join([f"{PERTURB_BY}"] * MAX_LOOPS_SUPPORTED))
+        # pypy default function counter
+        fp.write(','.join([f"{1619}"] * MAX_LOOPS_SUPPORTED))
         fp.write("\n")
-        fp.write(','.join([f"{PERTURB_BY}"] * MAX_LOOPS_SUPPORTED))
+        # pypy default bridge counter
+        fp.write(','.join([f"{PYPY_DEFAULT_BRIDGE}"] * MAX_LOOPS_SUPPORTED))
         fp.write("\n")
 
 if __name__ == "__main__":
@@ -136,7 +146,6 @@ if __name__ == "__main__":
         with open(STATS_FILE, "w") as fp:
             pass    
         for bench_name, inner_iterations in AWFY_BENCHMARKS.items():
-            initialize_loopfile()
             minimize(bench_name, inner_iterations)
         with open(STATS_FILE, "r") as fp:
             lines = fp.readlines()
